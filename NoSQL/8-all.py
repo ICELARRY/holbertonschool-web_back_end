783e943294be3092c3002052f54c609efb490d39
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2026-01-01 09:39:13 +0000

    Add 8-all.py

---
+++ NoSQL/8-all.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" 8-all.py """
+
+def list_all(mongo_collection):
+    """Return a list of all documents in a MongoDB collection"""
+    if mongo_collection is None:
+        return []
+    return list(mongo_collection.find())
