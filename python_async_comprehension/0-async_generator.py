247242bdf00185c37acef6da7cdaedbf62a100ab
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2026-01-03 15:19:18 +0000

    Add async generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 """
-Async generator that yields random numbers.
+Async generator that yields random numbers
 """
 
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """Asynchronously yield 10 random numbers between 0 and 10."""
     for _ in range(10):
         await asyncio.sleep(1)
