3f90090477f5f3156fb195e1b0f1ab34344f6d9a
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2026-01-10 20:24:16 +0000

    Add type-annotated add function

---
+++ python_variable_annotations/4-main.py
@@ -0,0 +1,5 @@
+bob@dylan:~$ ./4-main.py
+a is a <class 'int'> with a value of 1
+pi is a <class 'float'> with a value of 3.14
+i_understand_annotations is a <class 'bool'> with a value of True
+school is a <class 'str'> with a value of Holberton
