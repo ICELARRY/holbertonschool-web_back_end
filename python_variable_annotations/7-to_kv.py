62ee2718f190ed4bb5edf905aa2cc64c549b464b
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2026-01-10 20:33:30 +0000

    Add type-annotated add function

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -5,5 +5,8 @@ from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """Return a tuple where first element is k and second is square of v as float"""
+    """
+    Return a tuple where the first element is k and the second
+    element is the square of v as a float
+    """
     return (k, float(v ** 2))
